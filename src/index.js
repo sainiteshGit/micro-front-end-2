9894ab0951862e7803ace60f7e57712391267b54
sainiteshGit <nitesh_4u@yahoo.com>
2023-01-11 21:52:44 -0500

    update the micro front end 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+console.log('micro-front-end-2');
+document.querySelector('#microFrontend2').innerHTML=`<h1>Micro-Front-End-2</h1>`
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

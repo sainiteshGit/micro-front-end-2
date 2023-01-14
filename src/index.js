e4cbf8c6c0cd7c68b3036b4b48bcd61ce69f3d77
sainiteshGit <nitesh_4u@yahoo.com>
2023-01-13 21:58:25 -0500

    mfe-2 update files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 console.log('micro-front-end-2');
-document.querySelector('#microFrontend2').innerHTML=`<h1>Micro-Front-End-2 Update</h1>`
+document.querySelector('#microFrontend2').innerHTML=`<h1>Micro-Front-End-2 Update 2</h1>`
 
